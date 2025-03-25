782456eadb944bc1e34042d37a7462d16d4ce686
Md. Rakib Hossain <md.rakib@brainstation-23.com>
2025-03-25 20:02:18 +0600

    Did the web part.

---
+++ OnionEFCore/Controllers/StudentController.cs
@@ -0,0 +1,82 @@
+﻿using Core.Domain.Model;
+using Core.Repository.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository _studentRepository;
+        public StudentController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var students = await _studentRepository.GetAllAsync();
+            return View(students);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var student = await _studentRepository.GetStudentDetailsByIdAsync(id);
+            if (student == null)
+            {
+                return NotFound($"Student with {id} not found!");
+            }
+            return View(student);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                await _studentRepository.AddAsync(student);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var student = await _studentRepository.GetByIdAsync(id);
+            if (student == null)
+            {
+                return NotFound($"Student with {id} not found!");
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Student student)
+        {
+
+            if (ModelState.IsValid)
+            {
+                await _studentRepository.UpdateAsync(student);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _studentRepository.GetByIdAsync(id);
+            if (ModelState.IsValid)
+            {
+                await _studentRepository.DeleteAsync(student);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+    }
+}
